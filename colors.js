cf8326826d134205481c837fdc38a2dac2821f68
missingn <129937927+missingn@users.noreply.github.com>
2023-04-19 09:59:13 +0900

    Add files via upload

--- colors.js
+++ colors.js
@@ -1,31 +1,29 @@
  
-    var Body= {SetColor:function(color){$('body').css('color',color) },//글씨 색깔
+var Body= {SetColor:function(color){$('body').css('color',color) },//글씨 색깔
     BackSetColor:function(color){$('body').css('backgroundColor',color);}  //배경 색깔
-    
-
     }
 
  var link={asetColor:function(color) { $('a').css('color',color);}}
 
+ var boder={setbodercolor:function(color){$('div').css('borderColor',color)}}
 
 function NightDay(self){ 
 var target=document.querySelector('body');
 if(self.value === 'night' ) { 
 
-Body.BackSetColor('black') ;  
-Body.SetColor('white') ;  
+Body.BackSetColor("black") ;    //큰 따옴표랑 작은 따옴표 차이가 없네?
+Body.SetColor("white") ;  
 self.value='day';
 link.asetColor('yellow');
-document.getElementById("gri").style.borderColor = "white"; 
-document.getElementById("grid2").style.borderColor = "white"; 
+boder.setbodercolor('white'); 
+ 
 }
 
 else { Body.BackSetColor('white') ;
  Body.SetColor('black')
  self.value='night' ;
 link.asetColor('blue');
-document.getElementById("gri").style.borderColor = "black"; 
-document.getElementById("grid2").style.borderColor = "black"; 
+boder.setbodercolor('black');
 } 
 
 }
